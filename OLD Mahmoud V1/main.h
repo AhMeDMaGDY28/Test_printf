31224cf9a2551fd57e5015d4796802c29435b480
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-12 16:10:29 +0200

    test

--- OLD Mahmoud V1/main.h
+++ OLD Mahmoud V1/main.h
@@ -16,21 +16,11 @@ typedef struct specs
 	char *spec;
 	int (*f)();
 } specs;
-
 int _printf(const char *format, ...);
-<<<<<<<< HEAD:old_ahmed v01/main.h
-int _strlen(char *s);
-int _putchar(char c);
-int print_string(va_list args);
-int print_number(va_list args);
-
-
-========
-char *_puts(char *string);
 int _putchar(char c);
 int print_string(va_list args);
 int print_percent(void);
 int print_char(va_list args);
 int print_number(va_list args);
->>>>>>>> 34eecc6 (mahmoud old version):OLD Mahmoud V1/main.h
+
 #endif
